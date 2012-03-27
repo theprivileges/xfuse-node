97552370ac87c224f3f117d6be4d5a337dccdab6
Luiz Lopes <luizlopes@gmail.com>
2012-03-27 17:47:06 -0400

    added new js files

--- tests/test-xfuse.js
+++ tests/test-xfuse.js
@@ -1,37 +1,72 @@
 (function() {
-  var XFuse, client, postArgs;
+  var assert, events, siteUrl, token, vows, xfuse;
 
-  XFuse = require('../lib/xfuse.js').XFuse;
+  xfuse = require('../index.js');
 
-  postArgs = {
-    email: 'updated@email.com'
-  };
+  vows = require('vows');
 
-  client = new XFuse('46bb0a820d08bc7b1feeabce44e01beecb440253');
+  events = require('events');
 
-  exports['client is an Object'] = function(test) {
-    test.expect(2);
-    test.ok(client instanceof Object, "client is not an instance of Object");
-    test.equal(typeof client, 'object', "client is not an Object");
-    test.done();
-  };
+  assert = require('assert');
 
-  exports['test get'] = function(test) {
-    test.expect(1);
-    test.ok(client.get('/'));
-    test.done();
-  };
+  token = '46bb0a820d08bc7b1feeabce44e01beecb440253';
 
-  exports['test post'] = function(test) {
-    test.expect(1);
-    test.ok(client.post('/users/20', postArgs));
-    test.done();
-  };
+  siteUrl = 'mycompany.labs.memberfuse.com';
 
-  exports['test put'] = function(test) {
-    test.expect(1);
-    test.ok(client.put('/users', putArgs));
-    test.done();
-  };
+  vows.describe("xfuse.test").addBatch({
+    "Before we run anything": {
+      topic: function() {
+        return xfuse.setApiToken(null);
+      },
+      "API Token should be null": function(xfuse) {
+        return assert.isNull(xfuse.getApiToken());
+      }
+    }
+  }).addBatch({
+    "When accessing xfuse": {
+      "with no api token": {
+        "and looking for data": {
+          topic: function() {
+            return xfuse.get("/", this.callback);
+          },
+          "should get an error": function(err, res) {
+            return assert.include(res, "error");
+          }
+        }
+      },
+      "with an api token": {
+        topic: function() {
+          var promise;
+          promise = new events.EventEmitter();
+          xfuse.setApiToken(token);
+          xfuse.setSiteUrl(siteUrl);
+          xfuse.get("/", function(err, res) {
+            if (!res || res.error) {
+              promise.emit("error", err);
+              return console.error(err);
+            } else {
+              return promise.emit("success", res);
+            }
+          });
+          return promise;
+        },
+        /* Now we start doing some work
+        */
+        "response should have right resources": function(res, err) {
+          assert.isNull(err);
+          assert.include(res, "resources");
+        },
+        "and getting data from an user": {
+          topic: function() {
+            return xfuse.get("/user/20", this.callback);
+          },
+          "response should be valid": function(err, res) {
+            assert.isNull(err);
+            return assert.equal("20", res.user.id, "response id should be valid");
+          }
+        }
+      }
+    }
+  })["export"](module);
 
 }).call(this);
